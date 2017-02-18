0b558b75227a540a325b233b785e68109c24007b
kmyhy <MY_FANGXIAN@example.com>
2017-02-18 15:47:32 +0800

    Initial Commit

---
+++ Capsule/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Capsule
+//
+//  Created by qq on 2017/2/18.
+//  Copyright © 2017年 qq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
